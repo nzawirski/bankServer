a4ffd80be2709b5454ad5e1b3691fb3d6d079c30
Cardello9 <26511@student.pwsztar.edu.pl>
2019-05-29 11:55:50 +0200

    cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+var cors = require('cors');
+app.use(cors());
+
 const { Client } = require('pg')
 const client = new Client({
     user: "2018_zawirski_nikodem",
