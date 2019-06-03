f4967473c68b3eebc80e866d011bdd8cf69d5e9c
nzawirski <nikon96@gmail.com>
2019-06-03 17:21:28 +0200

    client edit

--- api/client.js
+++ api/client.js
@@ -17,10 +17,27 @@ router.get('/', verifyToken, (req, res) => {
             })
         } else {
             // user ok
-            console.log(authData);
-            res.json({
-                message: authData.user
-            })
+
+            const client = new Client(db)
+
+            let userId = authData.id
+            client.connect()
+            const query = `SELECT * FROM bank.klient
+                WHERE id_klienta = ${userId} `
+
+            client.query(query)
+                .then(qres => {
+                    res.json({
+                        message: qres.rows
+                    })
+                    client.end()
+                })
+                .catch(e => {
+                    console.error(e.stack)
+                    res.status(500).json({
+                        message: e.message
+                    })
+                })
 
         }
     })
@@ -99,6 +116,61 @@ router.post('/add', (req, res) => {
 
     })
 
+})
+
+//edycja klienta
+router.post('/edit', verifyToken, (req, res) => {
+
+    jwt.verify(req.token, config.get('secretKey'), (err, authData) => {
+        if (err) {
+            res.status(403).json({
+                message: err
+            })
+        } else {
+            const client = new Client(db)
+
+            let userId = authData.id
+            let imie = req.body.imie ? inQuotes(req.body.imie) : null
+            let nazwisko = req.body.nazwisko ? inQuotes(req.body.nazwisko) : null
+            let login = req.body.login ? inQuotes(req.body.login) : null
+            let id_oddzialu_banku = req.body.id_oddzialu_banku ? inQuotes(req.body.id_oddzialu_banku) : null
+
+
+            client.connect()
+
+            let query = `UPDATE bank.klient SET `
+
+            if (imie) {
+                query += `imie=${imie},`
+            }
+            if (nazwisko) {
+                query += `nazwisko=${nazwisko},`
+            }
+            if (login) {
+                query += `login=${login},`
+            }
+            if (id_oddzialu_banku) {
+                query += `id_oddzialu_banku=${id_oddzialu_banku},`
+            }
+            query = query.slice(0, -1) // cut the last coma
+            query += ' '
+
+            query += `WHERE id_klienta=${userId};`
+
+            console.log(query)
+            client.query(query)
+                .then(qres => {
+                    res.sendStatus(200)
+                    client.end()
+                })
+                .catch(e => {
+                    console.error(e.stack)
+                    res.status(500).json({
+                        message: e.message
+                    })
+                })
+        }
+    })
 })
 
 inQuotes = (string) => {
